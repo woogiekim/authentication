d845b44f5c8f3172644cb033d88dfa2e33a33875
Woogie Kim <mdir2kor@gmail.com>
2023-10-02 17:35:50 +0900

    Base32 인코딩 고정으로 변경

--- src/main/kotlin/org/auth/google/GoogleOtp.kt
+++ src/main/kotlin/org/auth/google/GoogleOtp.kt
@@ -5,7 +5,6 @@ import com.google.zxing.MultiFormatWriter
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import de.taimos.totp.TOTP
 import org.apache.commons.codec.binary.Base32
-import org.apache.commons.codec.binary.BaseNCodec
 import org.apache.commons.codec.binary.Hex
 import org.auth.DefaultSecretKeyGenerator
 import org.auth.SecretKeyGenerator
@@ -15,19 +14,21 @@ import java.awt.image.BufferedImage
 class GoogleOtp(
     val issuer: String,
     val account: String,
-    private val baseNCodec: BaseNCodec = Base32(),
     secretKeyGenerator: SecretKeyGenerator = DefaultSecretKeyGenerator()
 ) {
+    companion object {
+        val base32 = Base32()
+    }
 
     val secretKey: String = secretKeyGenerator.generate().run {
-        baseNCodec.encodeToString(this)
+        base32.encodeToString(this)
     }
 
     val otpUrl: String =
         "otpauth://totp/${"$issuer:$account".encode()}?secret=${secretKey.encode()}&issuer=${issuer.encode()}"
 
     fun generateOtpNumber(): String {
-        val bytes = this.baseNCodec.decode(this.secretKey)
+        val bytes = base32.decode(this.secretKey)
         val hexKey = Hex.encodeHexString(bytes)
 
         return TOTP.getOTP(hexKey)
